9ade8c7b55fddc1d0faff5bd5f5d05b9090edf76
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-08-06 19:39:29 -0300

    Add Dummy PrefabGUID to Spawnable class

--- Constants/Spawnable.cs
+++ Constants/Spawnable.cs
@@ -6,4 +6,5 @@ internal static class Spawnable {
   public static readonly PrefabGUID Slot = new(1278389141);
   public static readonly PrefabGUID SlotChest = new(-251472465);
   public static readonly PrefabGUID Lamp = new(-1023837449);
+  public static readonly PrefabGUID Dummy = new(-1128238456);
 }
