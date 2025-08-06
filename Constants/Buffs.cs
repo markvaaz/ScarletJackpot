25a5bbaec4e34a1ff3ea3300c57b6d0ba01f2b17
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-08-06 19:39:22 -0300

    Add new PrefabGUIDs for VictorySlotBuff and SlotGameBuff in Buffs class

--- Constants/Buffs.cs
+++ Constants/Buffs.cs
@@ -8,4 +8,6 @@ internal static class Buffs {
   public static readonly PrefabGUID Immaterial = new(1360141727);
   public static readonly PrefabGUID Invisibility = new(1880224358);
   public static readonly PrefabGUID VictoryVoiceLineBuff = new(503680755);
+  public static readonly PrefabGUID VictorySlotBuff = new(-992614152); // Example GUID, replace with actual if needed
+  public static readonly PrefabGUID SlotGameBuff = new(1723455773);
 }
