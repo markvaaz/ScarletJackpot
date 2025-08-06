bb1cf258144c042d6f2044d33eab0e93b50746b6
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-08-06 13:44:06 -0300

    Add SlotId constant and MAX_BET_MULTIPLIER property to Constants class

--- Constants/Constants.cs
+++ Constants/Constants.cs
@@ -9,10 +9,11 @@ internal static class Constants {
   public static readonly PrefabGUID WIN_INDICATOR = new(-113436752);
   public static readonly PrefabGUID SlotInteractBuff = new(1405487786);
   public static PrefabGUID SPIN_COST_PREFAB => new PrefabGUID(Plugin.Settings.Get<int>("CostPrefabGUID"));
+  public const string SlotId = "__ScarletJackpot.Slot__";
   public static int SPIN_MIN_AMOUNT => Plugin.Settings.Get<int>("MinAmount");
   public static int SPIN_MAX_AMOUNT => Plugin.Settings.Get<int>("MaxAmount");
-  public const string SlotId = "__ScarletJackpot.Slot__";
   public static float RTP_RATE => Plugin.Settings.Get<float>("RTPRate");
   public static float BASE_WIN_CHANCE => Plugin.Settings.Get<float>("BaseWinChance");
   public static bool ENABLE_RTP_CONTROL => Plugin.Settings.Get<bool>("EnableRTPControl");
+  public static float MAX_BET_MULTIPLIER => Plugin.Settings.Get<float>("MaxBetMultiplier");
 }
